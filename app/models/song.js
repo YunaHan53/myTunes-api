be5d1528431b48d540d5817eaabe99d56b3be0f1
YunaH <YunaH503@gmail.com>
2020-03-30 14:12:01 -0400

    Added song.js in model and routes
    Auth works but CRUD is not working just yet

---
+++ app/models/song.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  album: {
+    type: String,
+    required: false
+  },
+  year: {
+    type: Number,
+    min: [1900, 'Year must be valid'],
+    required: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Song', songSchema)
