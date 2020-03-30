e98a3c88a1e8ae1d6369d36210b41dd347a84e59
YunaH <YunaH503@gmail.com>
2020-03-30 15:07:32 -0400

    Added model and routes for songs
    Added song routes to server.js
    CRUD is now working with index, create, show, update and delete

--- app/routes/song_routes.js
+++ app/routes/song_routes.js
@@ -1,30 +1,13 @@
-// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
-// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
-
-// pull in Mongoose model for song
 const Song = require('../models/song')
 
-// this is a collection of methods that help us detect situations when we need
-// to throw a custom error
 const customErrors = require('../../lib/custom_errors')
-
-// we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
-// we'll use this function to send 401 when a user tries to modify a resource
-// that's owned by someone else
 const requireOwnership = customErrors.requireOwnership
 
-// this is middleware that will remove blank fields from `req.body`, e.g.
-// { song: { title: '', text: 'foo' } } -> { song: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
-// passing this as a second argument to `router.<verb>` will make it
-// so that a token MUST be passed for that route to be available
-// it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
-
-// instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
 // INDEX
@@ -35,9 +18,7 @@ router.get('/songs', requireToken, (req, res, next) => {
     .then(songs => {
       return songs.map(song => song.toObject())
     })
-    // respond with status 200 and JSON of the songs
     .then(songs => res.status(200).json({ songs: songs }))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
@@ -48,9 +29,7 @@ router.get('/songs/:id', (req, res, next) => {
   Song.findById(req.params.id)
     .then(handle404)
     // if `findById` is succesful, respond with 200 and "song" JSON
-    .then(song => {
-      res.status(200).json({ song: song.toObject() })
-    })
+    .then(song => res.status(200).json({ song: song.toObject() }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
@@ -58,11 +37,9 @@ router.get('/songs/:id', (req, res, next) => {
 // CREATE
 // POST /songs
 router.post('/songs', requireToken, (req, res, next) => {
-  // set owner of new song to be current user
   req.body.song.owner = req.user.id
 
   Song.create(req.body.song)
-  // respond to successful `create` with status 201 and JSON of new "song"
     .then(song => res.status(201).json({ song: song.toObject() }))
     .catch(next)
 })
@@ -70,8 +47,6 @@ router.post('/songs', requireToken, (req, res, next) => {
 // UPDATE
 // PATCH /songs/5a7db6c74d55bc51bdf39793
 router.patch('/songs/:id', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
   delete req.body.song.owner
 
   Song.findById(req.params.id)
@@ -80,13 +55,10 @@ router.patch('/songs/:id', requireToken, removeBlanks, (req, res, next) => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
       requireOwnership(req, song)
-
       // pass the result of Mongoose's `.update` to the next `.then`
       return Song.findOneAndUpdate({_id: req.params.id}, req.body.song, {new: true, runValidators: true})
     })
-    // if that succeeded, return 204 and no JSON
     .then(song => res.status(201).json({ song: song.toObject() }))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
@@ -101,9 +73,7 @@ router.delete('/songs/:id', requireToken, (req, res, next) => {
       // delete the song ONLY IF the above didn't throw
       song.deleteOne()
     })
-    // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
